f70710d1ccc745defcdc309c508c729da9ac7784
Alexandru Surdu Bob <alex.surdubob@gmail.com>
2022-11-28 21:58:05 +0200

    Test node functioning!

--- src/perception/src/laneDetection/laneDetectionNODE.py
+++ src/perception/src/laneDetection/laneDetectionNODE.py
@@ -10,19 +10,16 @@ import cv2 as cv
 class laneDetectionNODE():
     def __init__(self):
         # TODO docstring
-        rospy.loginfo('starting laneDetectionNODE - init')
         rospy.init_node('laneDetectionNODE', anonymous=False)
-        rospy.loginfo('starting laneDetectionNODE - init')
         rospy.Subscriber("/automobile/image_raw", Image, self._streams)
-
 
     def run(self):
         rospy.loginfo('starting laneDetectionNODE')
-        rospy.spin()
+        rospy.spin()    
 
     def imgmsg_to_cv2(self, img_msg):
-        if img_msg.encoding != "bgr8":
-            rospy.logerr("This Coral detect node has been hardcoded to the 'bgr8' encoding.  Come change the code if you're actually trying to implement a new camera")
+        # if img_msg.encoding != "bgr8":
+            # rospy.logerr("This Coral detect node has been hardcoded to the 'bgr8' encoding.  Come change the code if you're actually trying to implement a new camera")
         dtype = np.dtype("uint8") # Hardcode to 8 bits...
         dtype = dtype.newbyteorder('>' if img_msg.is_bigendian else '<')
         image_opencv = np.ndarray(shape=(img_msg.height, img_msg.width, 3), # and three channels of data. Since OpenCV works with bgr natively, we don't need to reorder the channels.
@@ -34,7 +31,13 @@ class laneDetectionNODE():
 
     def _streams(self, msg):
         image = self.imgmsg_to_cv2(msg)
-        cv.fastNlMeansDenoisingColored(image, None,10,10,7,21)
+        image = cv.cvtColor(image, cv.COLOR_RGB2GRAY)
+        image = cv.Canny(image,50,150,apertureSize = 3)
+        
         cv.imshow('Test', image)
-        cv.waitKey(0)
+        if cv.waitKey(1) == 27:
+            sys.exit(0)
 
+if __name__ == "__main__":
+    laneDetection = laneDetectionNODE()
+    laneDetection.run()
