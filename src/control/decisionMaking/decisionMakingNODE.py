ca7e0e0099e59dcf8341b87c064346f60530fa3d
Alexandru Surdu Bob <alex.surdubob@gmail.com>
2022-12-01 14:41:15 +0200

    Added message for lane detection

--- src/control/decisionMaking/decisionMakingNODE.py
+++ src/control/decisionMaking/decisionMakingNODE.py
@@ -2,24 +2,31 @@
 
 import rospy
 from std_msgs.msg import String
+from utils.msg import laneDetection
 import time
 
 class decisionMakingNODE:
     def __init__(self):
         rospy.init_node('decisionMakingNODE', anonymous=False)
         self.command_publisher = rospy.Publisher("/automobile/command", String, queue_size=1)
+        self.lane_info = rospy.Subscriber("/lane_info", laneDetection, self._lane)
 
-    def run(self):
-        rospy.loginfo('starting decisionMakingNODE')
-        time.sleep(2)
+    def move_forward(self):
         command = String()
         command.data = '{"action":"1","speed": 0.5}'
         self.command_publisher.publish(command)
         time.sleep(5)
         command.data = '{"action":"1","speed": 0.0}'
         self.command_publisher.publish(command)
-        # rospy.spin()
 
+    def run(self):
+        rospy.loginfo('starting decisionMakingNODE')
+        time.sleep(2)
+        self.move_forward()
+        rospy.spin()
+
+    def _lane(self, msg):
+        rospy.loginfo(msg)
 
 if __name__ == "__main__":
     decisionMaking = decisionMakingNODE()
