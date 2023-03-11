07a8126c9b04284cf8417f06ef498c90fc9fd368
Dana Dăscălescu <48893255+danadascalescu00@users.noreply.github.com>
2023-03-11 15:13:00 +0200

    Update laneDetectionNODE.py

--- src/perception/src/laneDetection/laneDetectionNODE.py
+++ src/perception/src/laneDetection/laneDetectionNODE.py
@@ -8,6 +8,126 @@ from perception.msg import lineArray, line
 
 import cv2 as cv
 
+"""
+    Variables declarations
+"""
+kernel_size = 3
+match_mask_color = 255 # value of lines' color
+
+# define range of black color in HSV color space
+
+# define Canny edge detection parameters
+low_threshold = 100
+high_threshold = 200
+
+# Hough transform parameters
+rho = 1             # distance precision in pixel
+angle = np.pi / 180 # angular precision in radian
+min_threshold = 10  # min number of votes for valid line
+
+
+"""
+    Functions definitions and declarations
+"""
+
+def preprocess_image(image):
+    # Convert image to grayscale
+    gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    # remove noise with gaussian blur
+    denoised_image = cv.GaussianBlur(gray_image, (kernel_size, kernel_size), 0)
+
+    return denoised_image
+
+
+def region_of_interest(edges_image):
+    height, width = edges_image.shape
+    mask = np.zeros_like(edges_image)
+
+    # only focus on the bottom half of the screen
+    polygon = np.array([[
+        (0, height * .5),
+        (width, height * .5),
+        (width, height),
+        (0, height)
+    ]], np.int32)
+
+    cv.fillPoly(mask, polygon, match_mask_color)
+    masked_image = cv.bitwise_and(edges_image, mask)
+
+    return masked_image
+
+
+def detect_segments(cropped_edges_image):
+    line_segments = cv.HoughLinesP(cropped_edges_image, rho, angle,
+                                    min_threshold, np.array([]),
+                                    minLineLength=15, # min allowed length of a single line
+                                    maxLineGap=15 # max allowed gap between line for joining them together
+                                    )
+    return line_segments
+
+
+def make_points(frame, line):
+    height, width, _ = frame.shape
+    slope, intercept = line
+    y1 = height # bottom of the frame
+    y2 = int(y1 * .5) # make points from bottom of the frame up
+
+    # bound the coordinates within the frame
+    x1 = max(-width, min(2 * width, int((y1 - intercept) / slope)))
+    x2 = max(-width, min(2 * width, int((y2 - intercept) / slope)))
+
+    return [[x1, y1, x2, y2]]
+
+
+def average_slope_intercept(frame, line_segments):
+    lane_lines = []
+    if line_segments is None:
+        return lane_lines
+
+    height, width, _ = frame.shape
+    left_fit, right_fit = [], []
+
+    boundary = 1 / 3
+    left_region_boundary = width * (1 - boundary) 
+    right_region_boundary = width * boundary
+
+    for line_segment in line_segments:
+        for x1, y1, x2, y2 in line_segment:
+            if x1 == x2: # slope = inf
+                continue
+
+            fit = np.polyfit((x1, x2), (y1, y2), 1)
+            slope, intercept = fit[0], fit[1]
+            if slope < 0:
+                if x1 < left_region_boundary and x2 < left_region_boundary:
+                    left_fit.append((slope, intercept))
+            else:
+                if x1 > right_region_boundary and x2 > right_region_boundary:
+                    right_fit.append((slope, intercept))
+
+
+    left_fit_average = np.average(left_fit, axis=0)
+    if len(left_fit) > 0:
+        lane_lines.append(make_points(frame, left_fit_average))
+
+    right_fit_average = np.average(right_fit, axis=0)
+    if len(right_fit) > 0:
+        lane_lines.append(make_points(frame, right_fit_average))
+
+    return lane_lines
+
+
+def draw_lines(image, lines, line_color=(0, 255, 255), line_width=10):
+    line_image = np.zeros_like(image)
+    if lines is not None:
+        for line in lines:
+            for x1, y1, x2, y2 in line:
+                cv.line(line_image, (x1, y1), (x2, y2), line_color, 1, 1)
+
+    new_image = cv.addWeighted(image, 0.8, line_image, 1, 1)
+    return new_image
+
+
 class laneDetectionNODE():
     def __init__(self):
         # TODO docstring
@@ -18,25 +138,36 @@ class laneDetectionNODE():
         cv.startWindowThread()
         cv.namedWindow("Test")
 
+
     def run(self):
         rospy.loginfo('starting laneDetectionNODE')
         rospy.spin()    
 
-    def imgmsg_to_cv2(self, img_msg):
 
+    def imgmsg_to_cv2(self, img_msg):
         dtype = np.dtype("uint8") # Hardcode to 8 bits...
         dtype = dtype.newbyteorder('>' if img_msg.is_bigendian else '<')
-        image_opencv = np.ndarray(shape=(img_msg.height, img_msg.width, 3), # and three channels of data. Since OpenCV works with bgr natively, we don't need to reorder the channels.
-                        dtype=dtype, buffer=img_msg.data)
+        image_opencv = np.ndarray(shape=(img_msg.height, img_msg.width, 3), # and three channels of data.
+                        dtype=dtype, buffer=img_msg.data) # Since OpenCV works with BGR natively, we don't need to reorder thechannels.
         # If the byt order is different between the message and the system.
         if img_msg.is_bigendian == (sys.byteorder == 'little'):
             image_opencv = image_opencv.byteswap().newbyteorder()
+
         return image_opencv
+
 
     def _streams(self, msg):
         image = self.imgmsg_to_cv2(msg)
         h, w, _ = image.shape
-        image = image[(h // 3):, :, :].copy()
+
+        # preprocess image
+        preprocessed_image = preprocess_image(image)
+        # Canny edge detection
+        edges_image = cv.Canny(preprocessed_image, low_threshold, high_threshold)
+        # extract the region of interest
+        roi_image = region_of_interest(edges_image)
+
+        # image = image[(h // 3):, :, :].copy()
         interest_field_view = image.copy()
         interest_field_view = cv.cvtColor(interest_field_view, cv.COLOR_RGB2GRAY)
         bilateral_filtered_imaged = cv.bilateralFilter(interest_field_view, 10, 75, 75)
@@ -48,15 +179,9 @@ class laneDetectionNODE():
         dilated_edges = cv.dilate(edges, kernel, iterations=1)
 
         # HoughLinesP method to directly obtain line end points
-        
-        lines_detected = cv.HoughLinesP(
-            dilated_edges, # input edge image,
-            1, # distance resolution in pixels
-            np.pi / 180, # angle resolution in radians
-            threshold = 50, # min number of votes for valid line
-            minLineLength = 30, # min allowed length of a single line
-            maxLineGap = 10, # max allowed gap between line for joining them together
-        )
+        lines_detected = detect_segments(roi_image)
+        lane_lines = average_slope_intercept(image, lines_detected)
+        line_image = draw_lines(image, lane_lines)
 
         message = lineArray()
         # Iterate over points coordinates
@@ -73,9 +198,11 @@ class laneDetectionNODE():
         
         self.lane_publisher.publish(message)
 
-        cv.imshow('Test', image)
+        cv.imshow('Test', line_image)
         if cv.waitKey(20) == 27:
             sys.exit(0)
+
+
 
 if __name__ == "__main__":
     laneDetection = laneDetectionNODE()
